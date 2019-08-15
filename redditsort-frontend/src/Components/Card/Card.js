9b1647abb471beb370de7a4460cfda64faa89b1a
Alexander Ward <alexanderward5@gmail.com>
2019-08-15 13:11:04 -0600

    added pagination to cards

--- redditsort-frontend/src/Components/Card/Card.js
+++ redditsort-frontend/src/Components/Card/Card.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
 import './Card.css';
 import CardRow from '../CardRow/CardRow';
+import Pagination from '../Pagination/Pagination';
 
 class Card extends Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      currentPage: 1,
+      postsPerPage: 5
     }
   }
+
+  setPage = (newPage) => {
+    this.setState({currentPage: newPage});
+  }
+
+  searchCard = (e) => {
+    console.log(e.target.value);
+  }
+
   render() {
-    const titles = this.props.titles.map((title, index) => (
+    const indexOfLastPost = this.state.currentPage * this.state.postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - this.state.postsPerPage;
+    const currentPosts = this.props.titles.slice(indexOfFirstPost, indexOfLastPost);
+    const titles = currentPosts.map((title, index) => (
       <CardRow mainText={title} key={index} />
     ));
     return (
       <div className="Card" >
         <div className="Card--content">
           <h3 className="Card--subreddit">{this.props.subreddit}</h3>
-          <input className="Card--search" type="text" placeholder={`Search ${this.props.subreddit}...`} />
+          <input 
+            className="Card--search" 
+            type="text" 
+            placeholder={`Search ${this.props.subreddit}...`} 
+            onChange={this.searchCard}
+          />
           <div className="Card--titles">
             {titles}
           </div>
+          {this.props.titles.length > 5 
+            &&
+            <Pagination totalPosts={this.props.titles.length} postsPerPage={this.state.postsPerPage} setPage={this.setPage} />
+          }
+          
         </div>
         
       </div>
---
+++ redditsort-frontend/src/Components/Pagination/Pagination.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Pagination.css';
+
+class Pagination extends Component {
+  
+  generateNumbers = () => {
+    const pageNumbers = [];
+    for (let i=1; i<=Math.ceil(this.props.totalPosts / this.props.postsPerPage); i++) {
+      pageNumbers.push(<button key={i} onClick={() => this.props.setPage(i)}>{i}</button>);
+    }
+    return pageNumbers;
+  }
+  
+  render() {
+    return (
+      <div className="Pagination">
+        {this.generateNumbers()}
+      </div>
+    );
+  }
+}
+
+export default Pagination;
